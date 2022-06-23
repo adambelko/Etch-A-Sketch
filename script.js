78ccaf67088e34316303b9fd92a85ad82cbc9132
Adzi <belkoadam@gmail.com>
2022-06-23 21:25:45 +1000

    fixed clear button function bg color change

--- script.js
+++ script.js
@@ -1,12 +1,55 @@
 
+let mouseIsDown;
+
+const settingsPanel = document.querySelector(".settings");
+const clearBtn = document.querySelector(".clear");
+settingsPanel.appendChild(clearBtn);
+
+const rangeBtn = document.querySelector(".divRange");
+settingsPanel.appendChild(rangeBtn);
+rangeBtn.addEventListener("click", getRange());
+
 function createDiv(num) {
     const gridContainer = document.querySelector(".gridContainer");
-    let mouseIsDown;
-
     for (i=0; i<(num*num); i++) {
         const div = document.createElement("div");
         div.setAttribute("class", "divItem");
 
+        div.addEventListener("click", e => div.style.backgroundColor = "black");
+        div.addEventListener("mousedown", e => mouseIsDown = true);
+        div.addEventListener("mouseup", e => mouseIsDown = false);
+
+        div.addEventListener("mousemove", e => {
+            if (mouseIsDown === true) {
+                div.style.backgroundColor = "black";
+            }
+        });
+        gridContainer.appendChild(div);
+    }
+    gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+}
+
+function getRange() {
+
+}
+
+function clearDiv() {
+        const x = document.querySelectorAll(".divItem");
+
+        clearBtn.addEventListener("click", e => {
+        for (i=0; i<x.length; i++) {
+            x[i].style.backgroundColor = "white";
+        }
+        getDefaultDiv();
+    });
+};
+
+
+function getDefaultDiv() {
+        const x = document.querySelectorAll(".divItem");
+
+        for (const div of x){
         div.addEventListener("click", e => e.target.classList.add("colorClassMove"));
         div.addEventListener("mousedown", e => mouseIsDown = true);
         div.addEventListener("mouseup", e => mouseIsDown = false);
@@ -16,26 +59,12 @@ function createDiv(num) {
                 e.target.classList.add("colorClassMove");
             }
         });
-        gridContainer.appendChild(div);
     }
-    gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
-    gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
 }
 
 createDiv(16);
 clearDiv();
 
-function clearDiv() {
-        const settingsPanel = document.querySelector(".settings");
-        const clearBtn = document.querySelector(".clear");
-        settingsPanel.appendChild(clearBtn);
-        const x = document.querySelectorAll(".divItem");
 
-        clearBtn.addEventListener("click", e => {
-        for (i=0; i<x.length; i++) {
-            x[i].style.backgroundColor = "white";
-        }
-    });
-};
+    
 
-    const range = document.querySelector(".rangeDiv");
