2fd7d302d74480678a21cc177b59c56d15831a90
Adzi <belkoadam@gmail.com>
2022-06-24 19:18:05 +1000

    added slider functionality for changing number of divs in a container

--- script.js
+++ script.js
@@ -2,15 +2,28 @@
 let mouseIsDown;
 
 const settingsPanel = document.querySelector(".settings");
-const clearBtn = document.querySelector(".clear");
-settingsPanel.appendChild(clearBtn);
 
-const rangeBtn = document.querySelector(".divRange");
-settingsPanel.appendChild(rangeBtn);
-rangeBtn.addEventListener("click", getRange());
+// gets vealue from slider to change grid div number
+function getValue() {
+    const rangeSelector = document.querySelector(".rangeSelector");
+    const currentValue = document.querySelector(".currentValue");
+    currentValue.textContent = `Grid size: ${rangeSelector.value} x ${rangeSelector.value}`;
+
+    rangeSelector.addEventListener("input", e => currentValue.textContent = `Grid size: ${rangeSelector.value} x ${rangeSelector.value}` );
+    rangeSelector.addEventListener("change", e => createDiv(rangeSelector.value));
+}
+
 
 function createDiv(num) {
     const gridContainer = document.querySelector(".gridContainer");
+
+    // if slider is moved to a different value all divs gets deleted so new ones can be created
+    if (num !== 31) {
+        while(gridContainer.firstChild) {
+            gridContainer.firstChild.remove();
+        }
+    }
+
     for (i=0; i<(num*num); i++) {
         const div = document.createElement("div");
         div.setAttribute("class", "divItem");
@@ -28,13 +41,13 @@ function createDiv(num) {
     }
     gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+
+    clearDiv();
 }
 
-function getRange() {
-
-}
 
 function clearDiv() {
+        const clearBtn = document.querySelector(".clear");
         const x = document.querySelectorAll(".divItem");
 
         clearBtn.addEventListener("click", e => {
@@ -46,24 +59,27 @@ function clearDiv() {
 };
 
 
+// brings back "painting" functionality to all divs 
 function getDefaultDiv() {
         const x = document.querySelectorAll(".divItem");
 
         for (const div of x){
-        div.addEventListener("click", e => e.target.classList.add("colorClassMove"));
+        div.addEventListener("click", e => div.style.backgroundColor = "black");
         div.addEventListener("mousedown", e => mouseIsDown = true);
         div.addEventListener("mouseup", e => mouseIsDown = false);
 
         div.addEventListener("mousemove", e => {
             if (mouseIsDown === true) {
-                e.target.classList.add("colorClassMove");
+                div.style.backgroundColor = "black";
             }
         });
     }
 }
 
-createDiv(16);
+createDiv(31);
+getValue();
 clearDiv();
+
 
 
     
