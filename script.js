2547ecde304386c1f99265b30fb9d291aaa038e2
Adzi <belkoadam@gmail.com>
2022-06-25 14:44:57 +1000

    re-positioned all EventListeners and querrySelectors out of individual functions so they're global

--- script.js
+++ script.js
@@ -1,22 +1,23 @@
 
 let mouseIsDown;
 
-const settingsPanel = document.querySelector(".settings");
+const gridContainer = document.querySelector(".gridContainer");
+const allDiv = document.querySelectorAll(".divItem");
 
-// gets vealue from slider to change grid div number
-function getValue() {
-    const rangeSelector = document.querySelector(".rangeSelector");
-    const currentValue = document.querySelector(".currentValue");
-    currentValue.textContent = `Grid size: ${rangeSelector.value} x ${rangeSelector.value}`;
+const clearBtn = document.querySelector(".clear");
+clearBtn.addEventListener("click", clearDiv);
 
-    rangeSelector.addEventListener("input", e => currentValue.textContent = `Grid size: ${rangeSelector.value} x ${rangeSelector.value}` );
-    rangeSelector.addEventListener("change", e => createDiv(rangeSelector.value));
-}
+const gridLinesBtn = document.querySelector(".gridLines");
+gridLinesBtn.addEventListener("click", changeGridLines);
 
+// gets value from slider to change number of divs to create a new grid
+const rangeSelector = document.querySelector(".rangeSelector");
+const currentValue = document.querySelector(".currentValue");
+currentValue.textContent = `Grid size: ${rangeSelector.value} x ${rangeSelector.value}`;
+rangeSelector.addEventListener("input", e => currentValue.textContent = `Grid size: ${rangeSelector.value} x ${rangeSelector.value}` );
+rangeSelector.addEventListener("change", e => createDiv(rangeSelector.value));
 
 function createDiv(num) {
-    const gridContainer = document.querySelector(".gridContainer");
-
     // if slider is moved to a different value all divs gets deleted so new ones can be created
     if (num !== 31) {
         while(gridContainer.firstChild) {
@@ -43,42 +44,55 @@ function createDiv(num) {
     gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
 
     clearDiv();
+    changeGridLines();
 }
 
+function eraseDiv() {
+    const div = document.querySelectorAll(".divItem");
+
+}
 
 function clearDiv() {
-        const clearBtn = document.querySelector(".clear");
-        const x = document.querySelectorAll(".divItem");
-
-        clearBtn.addEventListener("click", e => {
-        for (i=0; i<x.length; i++) {
-            x[i].style.backgroundColor = "white";
-        }
-        getDefaultDiv();
-    });
+    const allDiv = document.querySelectorAll(".divItem");
+    for (i=0; i<allDiv.length; i++) {
+        allDiv[i].style.backgroundColor = "white";
+    }
+    getDefaultDiv();
 };
 
 
-// brings back "painting" functionality to all divs 
+function changeGridLines() {
+    const allDiv = document.querySelectorAll(".divItem");
+    if (allDiv[0].style.border == "1px ridge black") {
+        for (i=0; i<allDiv.length; i++) {
+            allDiv[i].style.border = "0px";
+        }
+
+    } else {
+        for (i=0; i<allDiv.length; i++) {
+            allDiv[i].style.border = "1px ridge black";
+        }
+    }
+}
+
+
+// brings back "painting" functionality to all divs after "clearing" them
 function getDefaultDiv() {
-        const x = document.querySelectorAll(".divItem");
 
-        for (const div of x){
-        div.addEventListener("click", e => div.style.backgroundColor = "black");
-        div.addEventListener("mousedown", e => mouseIsDown = true);
-        div.addEventListener("mouseup", e => mouseIsDown = false);
+    for (i=0; i<allDiv.length; i++){
+        allDiv[i].addEventListener("click", e => div.style.backgroundColor = "black");
+        allDiv[i].addEventListener("mousedown", e => mouseIsDown = true);
+        allDiv[i].addEventListener("mouseup", e => mouseIsDown = false);
 
-        div.addEventListener("mousemove", e => {
+        allDiv[i].addEventListener("mousemove", e => {
             if (mouseIsDown === true) {
-                div.style.backgroundColor = "black";
+                allDiv[i].style.backgroundColor = "black";
             }
         });
     }
 }
 
 createDiv(31);
-getValue();
-clearDiv();
 
 
 
