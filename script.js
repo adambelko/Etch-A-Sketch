4b1d1266d0e0bcffeafc55f16b9b8d5911184970
Adzi <belkoadam@gmail.com>
2022-06-26 20:45:31 +1000

    added eraser functionality, need to add toggle effect to the Eraser button to disable functionality on next click

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-
 let mouseIsDown;
+let eraserClick;
 
 const gridContainer = document.querySelector(".gridContainer");
 const allDiv = document.querySelectorAll(".divItem");
+
+const eraseBtn = document.querySelector(".eraser");
+eraseBtn.addEventListener("click", e=> eraserClick = true);
 
 const clearBtn = document.querySelector(".clear");
 clearBtn.addEventListener("click", clearDiv);
@@ -10,12 +13,11 @@ clearBtn.addEventListener("click", clearDiv);
 const gridLinesBtn = document.querySelector(".gridLines");
 gridLinesBtn.addEventListener("click", changeGridLines);
 
-// gets value from slider to change number of divs to create a new grid
 const rangeSelector = document.querySelector(".rangeSelector");
 const currentValue = document.querySelector(".currentValue");
 currentValue.textContent = `Grid size: ${rangeSelector.value} x ${rangeSelector.value}`;
 rangeSelector.addEventListener("input", e => currentValue.textContent = `Grid size: ${rangeSelector.value} x ${rangeSelector.value}` );
-rangeSelector.addEventListener("change", e => createDiv(rangeSelector.value));
+rangeSelector.addEventListener("change", e => createDiv(rangeSelector.value)); // gets value from slider to change number of divs to create a new grid
 
 function createDiv(num) {
     // if slider is moved to a different value all divs gets deleted so new ones can be created
@@ -29,27 +31,24 @@ function createDiv(num) {
         const div = document.createElement("div");
         div.setAttribute("class", "divItem");
 
-        div.addEventListener("click", e => div.style.backgroundColor = "black");
+        div.addEventListener("click", e => {
+            div.style.backgroundColor = "black";
+            if (eraserClick === true) div.style.backgroundColor = "white";
+        });
         div.addEventListener("mousedown", e => mouseIsDown = true);
         div.addEventListener("mouseup", e => mouseIsDown = false);
 
         div.addEventListener("mousemove", e => {
-            if (mouseIsDown === true) {
-                div.style.backgroundColor = "black";
-            }
+            // need to make a toggle function for eraser button to disable changing bg color when not needed anumore
+            if (mouseIsDown === true && eraserClick === true) div.style.backgroundColor = "white";
+            else if (mouseIsDown === true) div.style.backgroundColor = "black";
         });
         gridContainer.appendChild(div);
     }
     gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
     gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
-
-    clearDiv();
+    
     changeGridLines();
-}
-
-function eraseDiv() {
-    const div = document.querySelectorAll(".divItem");
-
 }
 
 function clearDiv() {
@@ -80,14 +79,12 @@ function changeGridLines() {
 function getDefaultDiv() {
 
     for (i=0; i<allDiv.length; i++){
-        allDiv[i].addEventListener("click", e => div.style.backgroundColor = "black");
+        allDiv[i].addEventListener("click", e => div[i].style.backgroundColor = "black");
         allDiv[i].addEventListener("mousedown", e => mouseIsDown = true);
         allDiv[i].addEventListener("mouseup", e => mouseIsDown = false);
 
         allDiv[i].addEventListener("mousemove", e => {
-            if (mouseIsDown === true) {
-                allDiv[i].style.backgroundColor = "black";
-            }
+            if (mouseIsDown === true) div[i].style.backgroundColor = "black";
         });
     }
 }
